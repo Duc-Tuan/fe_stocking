fc9f8fe22ff0cddde205543da1406ebd6e6ddf25
Duc-Tuan <phamductuan26032002@gmail.com>
2025-08-30 15:18:18 +0700

    update

--- src/layouts/DashboardLayout.tsx
+++ src/layouts/DashboardLayout.tsx
@@ -12,9 +12,11 @@ import TooltipNavigate from './TooltipNavigate';
 import { dataHeader } from './type';
 import { useSocket } from '../hooks/useWebSocket';
 import { useAppInfo } from '../hooks/useAppInfo';
-import { Bounce, ToastContainer, toast as toastNotifi, type ToastContentProps } from 'react-toastify';
+import { Bounce, ToastContainer, toast as toastNotifi } from 'react-toastify';
 import { useTranslation } from 'react-i18next';
 import { titleSatusLot } from '../pages/Transaction/type';
+import { Button } from '../components/button';
+import { PathName } from '../routes/path';
 
 export default function DashboardLayout() {
     const { serverMonitorActive } = useAppInfo()
@@ -50,8 +52,15 @@ export default function DashboardLayout() {
     useEffect(() => {
         if (dataOrder) {
             if (dataOrder?.status === "open_order") {
-                toastNotifi.success(() => <CustomComponent data={dataOrder.data} />, {
-                    customProgressBar: true
+                // dataOrder.data
+                toastNotifi(() => <CustomComponent data={dataOrder.data} status={"open_order"}/>, {
+                    autoClose: 5000,
+                    icon: false,
+                })
+            } else {
+                toastNotifi(() => <CustomComponent data={dataOrder.data} status={"close_order"} />, {
+                    autoClose: 5000,
+                    icon: false,
                 })
             }
         }
@@ -76,14 +85,6 @@ export default function DashboardLayout() {
                 <Outlet />
             </main>
 
-            <button onClick={() => {
-                toastNotifi.success(CustomComponent, {
-                    autoClose: 5000,
-                    // removes the built-in progress bar
-                    customProgressBar: true
-                })
-            }}>OEIJD</button>
-
             <ToastContainer position="bottom-right"
                 autoClose={5000}
                 hideProgressBar={false}
@@ -104,26 +105,34 @@ const Logout = ({ handleclick }: { handleclick: () => void }) => {
     return <TooltipCustom classNameButton='shadow-md shadow-gray-500' w="w-[40px]" h='h-[40px]' handleClick={handleclick} titleTooltip={"Đăng xuất"}><Icon name="icon-logout" className="text-white " width={22} height={22} /></TooltipCustom>
 }
 
-function CustomComponent({ data }: { data: any }) {
+function CustomComponent({ data, status }: { data: any, status: "open_order" | "close_order" }) {
     const { t } = useTranslation()
+    const navigate = useNavigate(); // ⬅️ bạn bị thiếu dòng này
     return (
-        <div className="flex flex-col justify-center items-start gap-[2px] mt-3">
-            <h1>{t("Thông tin lô đã vào lệnh thành công")}</h1>
-            <div className="flex justify-start items-center gap-2">
-                <span>{t("Tài khoản theo dõi")}:</span>
-                <span className='font-semibold'>{data.account_monitor_id}</span>
-            </div>
+        <div className="flex w-full flex-col justify-center items-start gap-[2px] text-black">
+            <h1 className='font-bold text-[var(--color-background)]'>{status === "open_order" ? t("Lô đã vào lệnh thành công") : t("Lô đã đóng lệnh thành công")}</h1>
+            <div className="text-sm grid grid-cols-2 w-full">
+                <h2 className='col-span-2 font-bold'>{t("Tài khoản")}:</h2>
+                <div className="flex justify-start items-center gap-2 text-sm col-span-1 border-r border-r-gray-300">
+                    <span>{t("Theo dõi")}:</span>
+                    <span className='font-semibold'>{data.account_monitor_id}</span>
+                </div>
 
-            <div className="flex justify-start items-center gap-2">
-                <span>{t("Tài khoản giao dịch")}:</span>
-                <div className="flex justify-start items-center gap-1">
-                    <span className='font-semibold'>{data.account_transaction_id}</span>
-                    <Icon name="icon-chart-transaction" className="text-[var(--color-background)] mt-[2px]" width={18} height={18} />
+                <div className="flex justify-end items-center gap-2 text-sm col-span-1">
+                    <span>{t("Giao dịch")}:</span>
+                    <div className="flex justify-start items-center gap-1">
+                        <span className='font-semibold'>{data.account_transaction_id}</span>
+                        <Icon name="icon-chart-transaction" className="text-[var(--color-background)]" width={18} height={18} />
+                    </div>
                 </div>
             </div>
-            <div className="flex justify-start items-center gap-2">
-                <span>{t("Trạng thái")}: </span>
-                <span className='font-semibold'>{titleSatusLot(data.status_sl_tp)}</span>
+            <div className="flex justify-between items-center w-full mt-2">
+                <div className="flex justify-start items-center gap-2 text-sm">
+                    <span>{t("Trạng thái")}: </span>
+                    <span className='font-semibold'>{t(titleSatusLot(data.status_sl_tp))}</span>
+                </div>
+
+                <Button className='px-4 py-1 cursor-pointer bg-[var(--color-background)] rounded-sm text-sm shadow-gray-400' onClick={() => navigate(PathName.HISTORY)}>{t("Xem chi tiết")}</Button>
             </div>
         </div>
     );
